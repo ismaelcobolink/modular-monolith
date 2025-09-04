c1b28667e252f29bdaf0ac5639f98ac9515a7e48
Ismael Cobo <icobo@4link.app>
2025-09-04 09:04:54 +0200

    create common infrastructure project

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -1,6 +1,7 @@
 using Evently.Api.Extensions;
 using Evently.Modules.Events.Infrastructure;
 using Evently.Common.Application;
+using Evently.Common.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddApplication([
     Evently.Modules.Events.Application.AssemblyReference.Assembly
 ]);
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
 
 builder.Services.AddEventsModule(builder.Configuration);
 
