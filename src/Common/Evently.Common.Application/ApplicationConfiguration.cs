712bb91f11331ec7bc119f5d52bdc6bd7e82c972
Ismael Cobo <icobo@4link.app>
2025-09-04 12:06:33 +0200

    add validations

--- src/Common/Evently.Common.Application/ApplicationConfiguration.cs
+++ src/Common/Evently.Common.Application/ApplicationConfiguration.cs
@@ -15,8 +15,8 @@ public static class ApplicationConfiguration
             config.RegisterServicesFromAssemblies(moduleAssemblies);
 
             config.AddOpenBehavior(typeof(Behaviors.ExceptionHandlingPipelineBehavior<,>));
-
             config.AddOpenBehavior(typeof(Behaviors.RequestLoggingPipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(Behaviors.ValidationPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssemblies(moduleAssemblies, includeInternalTypes: true);
